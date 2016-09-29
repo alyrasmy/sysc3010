fe6157f83caa3c412eb5295f2368b1e1f9e56b1b
alyrasmy <alyrasmy94@gmail.com>
2016-09-29 16:02:39 -0400

    Update AddressBook.java
    lab2 changes from github

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -27,7 +27,7 @@ public class AddressBook {
 	public static void main(String[] args) {
 		AddressBook addresses = new AddressBook();
 		BuddyInfo buddyInfo = new BuddyInfo();
-		buddyInfo.setName("Aly2");
+		buddyInfo.setName("Aly3");
 		addresses.addBuddy(buddyInfo);
 	}
 }
